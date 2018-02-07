2bf3889c4446e4591d6e549059d270ca123e323b
天川之约 <woaitianwen@qq.com>
2018-02-07 23:08:30 +0800

    持久层实现类--更新用户名

--- src/test/java/com/zyl/water/WaterApplicationTests.java
+++ src/test/java/com/zyl/water/WaterApplicationTests.java
@@ -1,16 +1,60 @@
 package com.zyl.water;
 
+import com.zyl.water.controller.HelloController;
+import com.zyl.water.controller.UserController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class WaterApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(
+				new HelloController(),
+				new UserController()).build();
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
+	}
+
+	@Test
+	public void testUserController() throws Exception {
+//  	测试UserController
+		RequestBuilder request = null;
+
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[]")));
+
 	}
 
 }
