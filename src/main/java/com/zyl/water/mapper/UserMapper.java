812d295eb633a154a29f26bcb7fe0a17ee79e9c9
天川之约 <woaitianwen@qq.com>
2018-02-07 17:27:25 +0800

    User持久层接口类--查询全部用户接口

--- src/main/java/com/zyl/water/mapper/UserMapper.java
+++ src/main/java/com/zyl/water/mapper/UserMapper.java
@@ -1,11 +1,20 @@
 package com.zyl.water.mapper;
 
+import com.zyl.water.dao.User;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * Created by woaitianwen on 2018/2/1.
  */
 @Mapper
 public interface UserMapper {
+
+    /**
+     * 查询全部用户
+     * @return
+     */
+    List<User> selectAllUser();
 
 }
