9924c9a58d2a7c060bf8a7295e9fb076318c05ed
天川之约 <woaitianwen@qq.com>
2018-02-07 23:10:09 +0800

    User业务层--更新用户名

--- src/main/java/com/zyl/water/service/UserService.java
+++ src/main/java/com/zyl/water/service/UserService.java
@@ -5,6 +5,9 @@ import com.zyl.water.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -22,8 +25,35 @@ public class UserService {
         return userMapper.findAll();
     }
 
+    /**
+     * 新增用户
+     * @param user
+     */
     public void addUser(User user) {
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis()); //生成时间
+
+        //获取本地IP
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String ip = address.getHostAddress();
+
+        user.setReg_time(timestamp);
+        user.setReg_ip(ip);
+        user.setLast_login_time(timestamp);
+        user.setLast_login_ip(ip);
+        user.setUpdate_time(timestamp);
         userMapper.addUser(user);
+    }
+
+    public void updateUser(User user) {
+
+        userMapper.updateUser(user);
     }
 
 
