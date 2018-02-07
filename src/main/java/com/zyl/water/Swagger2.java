4979e5b5730be6bb31c6ceb120a1ea189ef68a4c
天川之约 <woaitianwen@qq.com>
2018-02-07 17:54:48 +0800

    增加Swagger2的API文档

---
+++ src/main/java/com/zyl/water/Swagger2.java
@@ -0,0 +1,43 @@
+package com.zyl.water;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author zhouyulong
+ * @version 1.0.0
+ * @date 18/2/7 下午5:50.
+ * @blog http://www.yulong360.com
+ */
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.didispace.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("请查看更多关于羽翼博客的文章")
+                .termsOfServiceUrl("http://www.yulong360.com")
+                .version("1.0")
+                .build();
+    }
+
+}
--- src/main/java/com/zyl/water/controller/UserController.java
+++ src/main/java/com/zyl/water/controller/UserController.java
@@ -2,9 +2,12 @@ package com.zyl.water.controller;
 
 import com.zyl.water.dao.User;
 import com.zyl.water.mapper.UserMapper;
+import com.zyl.water.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,15 +17,17 @@ import java.util.List;
  */
 @RestController
 @EnableAutoConfiguration
+@RequestMapping(value="/users")
 public class UserController {
 
     @Autowired
     UserMapper userMapper;
 
     @Autowired
-    UserServiceImpl userService;
+    UserService userService;
 
-    @GetMapping(value = "/users")
+    @ApiOperation(value="获取用户列表", notes="")
+    @RequestMapping(value={""}, method= RequestMethod.GET)
     List<User> getAllUser() {
         return userMapper.selectAllUser();
     }
