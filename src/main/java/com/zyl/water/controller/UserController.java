e25a29636d26d9ae5f5f8c4681550cedceb4c15b
天川之约 <woaitianwen@qq.com>
2018-02-07 17:24:39 +0800

    获取所有用户

--- src/main/java/com/zyl/water/controller/UserController.java
+++ src/main/java/com/zyl/water/controller/UserController.java
@@ -1,4 +1,31 @@
 package com.zyl.water.controller;
 
+import com.zyl.water.dao.User;
+import com.zyl.water.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by woaitianwen on 2018/2/1.
+ */
+@RestController
+@EnableAutoConfiguration
 public class UserController {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @GetMapping(value = "/users")
+    List<User> getAllUser() {
+        return userMapper.selectAllUser();
+    }
+
+
 }
