e6346650929f919c0dbe95a5f1de46a9265a01ff
天川之约 <woaitianwen@qq.com>
2018-02-08 14:28:27 +0800

    增加商品品牌实体类

---
+++ src/main/java/com/zyl/water/domain/GoodsBrand.java
@@ -0,0 +1,73 @@
+package com.zyl.water.domain;
+
+public class GoodsBrand {
+    private Integer brandId;
+
+    private String brandName;
+
+    private String brandTitile;
+
+    private String brandRebate;
+
+    private Integer status;
+
+    private Integer sort;
+
+    private String brandImg;
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName == null ? null : brandName.trim();
+    }
+
+    public String getBrandTitile() {
+        return brandTitile;
+    }
+
+    public void setBrandTitile(String brandTitile) {
+        this.brandTitile = brandTitile == null ? null : brandTitile.trim();
+    }
+
+    public String getBrandRebate() {
+        return brandRebate;
+    }
+
+    public void setBrandRebate(String brandRebate) {
+        this.brandRebate = brandRebate == null ? null : brandRebate.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+
+    public String getBrandImg() {
+        return brandImg;
+    }
+
+    public void setBrandImg(String brandImg) {
+        this.brandImg = brandImg == null ? null : brandImg.trim();
+    }
+}
