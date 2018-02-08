4572f8574f20f3f9af9b0c87aba82ad12a35d99b
天川之约 <woaitianwen@qq.com>
2018-02-08 15:31:33 +0800

    商品品牌控制层--查询商品品牌分页

--- src/main/java/com/zyl/water/controller/GoodsBrandController.java
+++ src/main/java/com/zyl/water/controller/GoodsBrandController.java
@@ -1,29 +1,38 @@
 package com.zyl.water.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.Page;
 import com.zyl.water.domain.GoodsBrand;
+import com.zyl.water.mapper.GoodsBrandMapper;
 import com.zyl.water.service.GoodsBrandService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 public class GoodsBrandController {
+
+    @Autowired
+    GoodsBrandMapper goodsBrandMapper;
 
     @Autowired
     GoodsBrandService goodsBrandService;
 
     @ApiOperation(value="查询所有商品品牌", notes="查询所有商品品牌")
     @RequestMapping(value = "brand",method = RequestMethod.GET)
-    public Object selectBybrand() {
-        List<GoodsBrand> goodsBrandList = goodsBrandService.selectBybrand();
+    public Object getByPage(@RequestParam(value = "pageNo",  defaultValue = "1")  int pageNo,
+                            @RequestParam(value = "pageSize", defaultValue = "3")  int pageSize) {
+        int totalCount = goodsBrandMapper.selectCount();
+        Page<GoodsBrand> goodsBrandPage = goodsBrandService.findByPage(pageNo, pageSize);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("msg","查询成功");
-        jsonObject.put("data",goodsBrandList);
+        jsonObject.put("total",totalCount);
+        jsonObject.put("data",goodsBrandPage);
         return jsonObject;
+
     }
 }
